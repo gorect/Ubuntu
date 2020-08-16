999f0626ce488b483dcee99947b1cb4ebc610d46
Cameron Gore <gorect@vcu.edu>
2020-08-16 18:51:44 -0400

    Update Docker.sh

--- Ubuntu-Server-20.04/Docker.sh
+++ Ubuntu-Server-20.04/Docker.sh
@@ -5,3 +5,6 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+##Update the apt package index, and install the latest version of Docker Engine and containerd, or go to the next step to install a specific version:
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
