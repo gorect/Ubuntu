13463c9f875f294716a58aeb9929740da84781b1
Cameron Gore <gorect@vcu.edu>
2020-08-16 18:48:55 -0400

    Update Docker.sh

--- Ubuntu-Server-20.04/Docker.sh
+++ Ubuntu-Server-20.04/Docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+##Use the following command to set up the stable repository. 
+   ##To add the nightly or test repository, add the word nightly or test (or both) after the word stable in the commands below.
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
