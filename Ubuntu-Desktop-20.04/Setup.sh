d4877c01d3948ab22f04582f5f3249f317094381
Cameron Gore <gorect@vcu.edu>
2021-09-15 10:21:38 -0400

    Update Setup.sh

--- Ubuntu-Desktop-20.04/Setup.sh
+++ Ubuntu-Desktop-20.04/Setup.sh
@@ -28,10 +28,9 @@ echo $PATH
 
 #install etcher
 echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
-sudo apt update -y
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+sudo apt update -y 
 sudo apt install balena-etcher-electron -y
-
 
 #set up grub for 'loud splash'
 # /etc/default/grub
