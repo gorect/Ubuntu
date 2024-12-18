007b81f74b7600c3db79a8c7a09b155e117ad40f
Cameron Gore <22473121+gorect@users.noreply.github.com>
2024-12-17 23:01:18 -0500

    Update Rancher-Persistent.sh
    #!/usr/bin/env bash

--- Server-20.04/Rancher-Persistent.sh
+++ Server-20.04/Rancher-Persistent.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ##Rancher uses etcd as datastore. When using the Docker Install, the embedded etcd is being used. 
   ##The persistent data is at the following path in the container: /var/lib/rancher. 
   ##You can bind mount a host volume to this location to preserve data on the host it is running on.
