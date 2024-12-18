621ea036fdb4d32cc095d5a71ed63313f3bbdb90
Cameron Gore <22473121+gorect@users.noreply.github.com>
2024-12-17 23:00:41 -0500

    Update Docker.sh
    #!/usr/bin/env bash

--- Server-20.04/Docker.sh
+++ Server-20.04/Docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ##Use the following command to set up the stable repository. 
    ##To add the nightly or test repository, add the word nightly or test (or both) after the word stable in the commands below.
 sudo add-apt-repository \
