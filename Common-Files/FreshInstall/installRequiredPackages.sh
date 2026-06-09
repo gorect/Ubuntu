e486dffdfb7f7d3442d6ee1405ea2cc45f06b5c5
Cameron Gore <22473121+gorect@users.noreply.github.com>
2026-06-09 00:11:30 +0000

    fixed branch issue

--- Common-Files/FreshInstall/installRequiredPackages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#set user check for VM
-# Is this a virtual machine?
-#if so, sudo apt install qemu-guest-agent
-
-sudo apt update && sudo apt upgrade -y
-sudo apt install vim wget curl net-tools cifs-utils gedit iputils-ping
